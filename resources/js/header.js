a22f2d98f56f61ea6b7f24a1833bc10039e4ae59
smrobert09 <smrobert09@gmail.com>
2014-02-10 23:43:17 -0700

    here we go

---
+++ resources/js/header.js
@@ -0,0 +1,7 @@
+document.title += ' - H';
+
+var header = '<nav><ul><li><a href="#">Home-ology</a></li><li><a href="tech.html">Tech-ology</a></li><li><a href="#">Craft-ology</a></li><li><a href="#">Blog-ology</a></li><li><a href="about.html">Bio-ology</a></li><li><a href="getoff.html">Dare-ology</a></li><li><a href="getoff2.html">Dare-ology 2</a></li></ul></nav>';
+var insertHeader = function(){
+	document.getElementsByTagName('header')[0].innerHTML = header;
+}
+insertHeader();
